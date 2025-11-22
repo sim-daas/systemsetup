b777ac39b2abec0fdd11286f2b35230dea5c716e
ubuntu <aitechmanml@gmail.com>
2025-11-22 12:07:03 +0530

    minor changes

---
+++ break_timer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while true; do
+    # Wait for 20 minutes (1200 seconds)
+    sleep 1200
+
+    # Send the notification
+    notify-send "Time to take a 20-minute eye break!" -u normal -t 5000
+done
